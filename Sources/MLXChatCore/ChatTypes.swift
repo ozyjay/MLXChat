2a378eb9cca450d21612346f970dd978ddcc690d
Jason Holdsworth <jason.holdsworth@jcu.edu.au>
2026-06-18 20:58:38 +1000

    Expose chat usage and diffusion metadata

--- Sources/MLXChatCore/ChatTypes.swift
+++ Sources/MLXChatCore/ChatTypes.swift
@@ -15,11 +15,25 @@ public struct ChatCompletionResult: Equatable, Sendable {
     public let assistantText: String
     public let statusCode: Int
     public let rawBody: Data
+    public let finishReason: String?
+    public let usage: ChatTokenUsage?
+    public let diffusion: TextDiffusionResultMetadata?
 
-    public init(model: String, assistantText: String, statusCode: Int, rawBody: Data) {
+    public init(
+        model: String,
+        assistantText: String,
+        statusCode: Int,
+        rawBody: Data,
+        finishReason: String? = nil,
+        usage: ChatTokenUsage? = nil,
+        diffusion: TextDiffusionResultMetadata? = nil
+    ) {
         self.model = model
         self.assistantText = assistantText
         self.statusCode = statusCode
         self.rawBody = rawBody
+        self.finishReason = finishReason
+        self.usage = usage
+        self.diffusion = diffusion
     }
 }
